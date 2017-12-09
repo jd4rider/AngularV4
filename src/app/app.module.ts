fb560f3cbc674a07d6a0e37106d9324255703645
Jonathan Forrider <jd4rider@gmail.com>
2017-12-09 02:23:14 -0700

    Got Video List Working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { VidoelistComponent } from './vidoelist/vidoelist.component';
 import { VideoListComponent } from './video-list/video-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    VidoelistComponent,
     VideoListComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [VideoListComponent]
 })
 export class AppModule { }
